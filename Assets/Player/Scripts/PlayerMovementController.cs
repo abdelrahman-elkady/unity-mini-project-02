b3a2f845b2bdf294d08438ec91dcf1952cc8b6d0
kady <elkadyabdelrahman@gmail.com>
2016-11-17 17:11:15 +0200

    Basic animation trails, Root Motion is having some problems

--- Assets/Player/Scripts/PlayerMovementController.cs
+++ Assets/Player/Scripts/PlayerMovementController.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class PlayerMovementController : MonoBehaviour {
 
+    private Animator mAnimator;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        mAnimator = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        mAnimator.SetFloat("vSpeed", Input.GetAxis("Vertical"));
 	}
 }
